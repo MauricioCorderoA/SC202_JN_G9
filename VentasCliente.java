d985d6823e8e29d378056b2fb61b258c21b73e55
Noilany Martinez Mora <maymm.zyn@gmail.com>
2025-07-24 20:01:03 -0600

    Update VentasCliente.java
    se cambiaron los atributos a privados

--- VentasCliente.java
+++ VentasCliente.java
@@ -2,12 +2,12 @@
  // Contiene los datos básicos de cada transaccion
 
 public class VentasCliente {
-    public String idVenta;       // Numero unico que identifica la venta
-    public String fechaVenta;   // Fecha en formato String (2026-11-20)
-    public int cantidadBoletos;  // Cantidad de boletos comprados
-    public double montoTotal;    // Monto de la transaccion 
-    public String estado;        // Estado de la venta: "activa", "cancelada", "completada"
-    public String metodoPago;    // Metodo de pago: "efectivo", "tarjeta", "transferencia"
-    public String numeroVuelo;   // Número de vuelo asociado ("IDvuelo")
+    private String idVenta;       // Numero unico que identifica la venta
+    private String fechaVenta;   // Fecha en formato String (2026-11-20)
+    private int cantidadBoletos;  // Cantidad de boletos comprados
+    private double montoTotal;    // Monto de la transaccion 
+    private String estado;        // Estado de la venta: "activa", "cancelada", "completada"
+    private String metodoPago;    // Metodo de pago: "efectivo", "tarjeta", "transferencia"
+    private String numeroVuelo;   // Número de vuelo asociado ("IDvuelo")
 }
 
