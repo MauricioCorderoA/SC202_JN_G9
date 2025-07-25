4a6fbd2ec309b8c974eb4579e13b3c797a061095
maugamer554@gmail.com <maugamer554@gmail.com>
2025-07-24 21:18:18 -0600

    avance 2

---
+++ SistemaVueloProyecto_Grupo9[1]/SistemaVueloProyecto_Grupo9/SistemaVuelo/src/main/java/com/mycompany/sistemavuelo/Administrador.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.sistemavuelo;
+
+/**
+ *
+ * @author Maytan
+ */
+public class Administrador {
+    
+    //ATRIBUTOS
+    private final String idAdministrador;
+    private final String contrasenaAdmin;
+    private String nombreAdmin;
+    
+    //CONSTRUCTOR
+    public Administrador(String idAdministrador, String contrasenaAdmin) {
+        this.idAdministrador = idAdministrador;
+        this.contrasenaAdmin = contrasenaAdmin;
+    }
+    
+    //ENCAPSULADORES
+    public String getIdAdministrador() {
+        return idAdministrador;
+    }
+    
+    public String getNombreAdmin(){
+        return nombreAdmin;
+    }
+    
+    public void setNombreAdmin(String nombreAdmin){
+        this.nombreAdmin=nombreAdmin;
+    }
+
+    public String getContrasenaAdmin() {
+        return contrasenaAdmin;
+    }
+    
+    //METODO PARA ENSEÑAR EL USUARIO AL INICIO DEL PROGRAMA
+    public void MostrarDatosAdmin(){
+        
+        System.out.println("ID: "+idAdministrador);
+        System.out.println("Contrasena: "+contrasenaAdmin);
+        System.out.println("-- -- --- -- --");
+    }
+    
+}
